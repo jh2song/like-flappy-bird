a2a79cb426640b4aa5002923ecef100da82aa70c
Jihoon Song <spec0003@naver.com>
2024-03-02 05:55:19 +0900

    [BACKUP]

---
+++ Assets/Scripts/Manager/Contents/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SpawnManager
+{
+    public Object Pillar;
+
+    public int[] OffsetX = { 10, 15, 20 };
+    public int[] OffsetY = { -1, 0, 1 };
+
+    public void Init()
+    {
+        Pillar = Resources.Load("Prefabs/Pillar");
+    }
+
+    public void Spawn()
+    {
+        int xIdx = Random.Range(0, OffsetX.Length);
+        int yIdx = Random.Range(0, OffsetY.Length);
+        Vector3 offsetVec = new Vector3(OffsetX[xIdx], OffsetY[yIdx], 0);
+
+        GameObject.Instantiate(Pillar, Vector3.zero + offsetVec, Quaternion.identity);
+    }
+}
--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -7,7 +7,7 @@ public class Manager : MonoBehaviour
     public static Manager instance = null;
 
     #region Contents
-    private Spawner _spawner = new Spawner();
+    private SpawnManager _spawnerMgr = new SpawnManager();
     #endregion
 
     // 싱글턴화
@@ -40,5 +40,14 @@ public class Manager : MonoBehaviour
     private void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        // 2초 마다 _spawnerMgr의 Spawn()을 호출
+        // InvokeRepeating 사용
+        
+        // Invoke 방식은 여기에 적용할 수 없음
+        // 코루틴 사용
     }
 }
